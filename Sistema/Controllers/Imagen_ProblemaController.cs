d56bd08a7d182b322c6c2307d4249d97d6b65965
Jon-San <81579522+Jon-San@users.noreply.github.com>
2023-06-28 19:01:36 -0500

    V34

---
+++ Sistema/Controllers/Imagen_ProblemaController.cs
@@ -0,0 +1,41 @@
+﻿using SistemaArtemis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaArtemis.Controllers
+{
+    public class Imagen_ProblemaController : Controller
+    {
+        Model1 db = new Model1();
+        private Imagen_Problema objImagenProblema = new Imagen_Problema();
+        // GET: Imagen_Problema
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SubirImagenProblema(int id)
+        {
+            ViewBag.Id_Problema = id;
+            return View(id);
+        }
+
+        [HttpPost]
+        public ActionResult GuardarImagen(int id_Problema, HttpPostedFileBase[] fileInput)
+        {
+            Imagen_Problema.GuardarImagen(id_Problema, fileInput);
+            return RedirectToAction("SubirImagenProblema", new { id = id_Problema });
+        }
+
+        public ActionResult VisualizarImagenes(int id_Problema)
+        {
+            var imagenes = objImagenProblema.VisualizarImagenes(id_Problema);
+            return View(imagenes);
+        }
+
+    }
+}
--- Sistema/Models/Imagen_Problema.cs
+++ Sistema/Models/Imagen_Problema.cs
@@ -4,7 +4,11 @@
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.IO;
+    using System.Linq;
+    using System.Web;
 
     public partial class Imagen_Problema
     {
@@ -22,5 +26,96 @@
         public int? Id_Problema { get; set; }
 
         public virtual Problema Problema { get; set; }
+
+        public static void GuardarImagen(int id_Problema, HttpPostedFileBase[] fileInput)
+        {
+            if (fileInput != null && fileInput.Length > 0)
+            {
+                using (var dbContext = new Model1()) 
+                {
+                    try
+                    {
+                        foreach (var file in fileInput)
+                        {
+                            if (file != null && file.ContentLength > 0)
+                            {
+                                var imagen = new Imagen_Problema();
+                                imagen.Nombre = Path.GetFileNameWithoutExtension(file.FileName);
+                                imagen.Extension = Path.GetExtension(file.FileName);
+                                imagen.Id_Problema = id_Problema;
+
+                                using (var reader = new BinaryReader(file.InputStream))
+                                {
+                                    imagen.Archivo = reader.ReadBytes(file.ContentLength);
+                                }
+
+                                dbContext.Imagen_Problema.Add(imagen);
+                            }
+                        }
+
+                        dbContext.SaveChanges();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public List<Imagen_Problema> VisualizarImagenes(int id_Problema)
+        {
+            var imagenes = new List<Imagen_Problema>();
+            try
+            {
+                using (var db = new Model1())
+                {
+                    imagenes = db.Imagen_Problema
+                        .Include("Problema")
+                        .Where(x => x.Id_Problema == id_Problema)
+                        .ToList();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return imagenes;
+        }
+
+
+
+
+        //public static void GuardarImagen(int id_Problema, HttpPostedFileBase[] fileInput)
+        //{
+        //    if (fileInput != null && fileInput.Length > 0)
+        //    {
+        //        using (var dbContext = new Model1()) 
+        //        {
+        //            foreach (var file in fileInput)
+        //            {
+        //                if (file != null && file.ContentLength > 0)
+        //                {
+        //                    var imagen = new Imagen_Problema();
+        //                    imagen.Nombre = Path.GetFileNameWithoutExtension(file.FileName);
+        //                    imagen.Extension = Path.GetExtension(file.FileName);
+        //                    imagen.Id_Problema = id_Problema;
+
+        //                    using (var reader = new BinaryReader(file.InputStream))
+        //                    {
+        //                        imagen.Archivo = reader.ReadBytes(file.ContentLength);
+        //                    }
+
+        //                    dbContext.Imagen_Problema.Add(imagen);
+        //                }
+        //            }
+
+        //            dbContext.SaveChanges();
+        //        }
+        //    }
+        //}
+
+
+
     }
 }
