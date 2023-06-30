3c30c8193e822ae37c468bf96edb7b2e9624f7f3
Deivis Jhonatan Flores Navarro <deifloresn@upt.pe>
2023-06-30 14:12:41 -0500

    V36

--- Sistema/Controllers/CalificacionController.cs
+++ Sistema/Controllers/CalificacionController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SistemaArtemis.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +11,83 @@ namespace SistemaArtemis.Controllers
     public class CalificacionController : Controller
     {
         // GET: Calificacion
+        static string cadena = "Data Source=.;Initial Catalog=ArtemisBD;Integrated security=true";
+
+        Model1 db = new Model1();
+
+        private Calificacion objcalifica = new Calificacion();
+
         public ActionResult Index()
         {
+            var listServicio = db.Servicio.Include("Tecnico").Where(s => s.Id_Estado_Servicio.Equals(3)).ToList();
+            ViewBag.Service = listServicio;
             return View();
         }
+
+
+        public ActionResult Calificar(int id = 0)
+        {
+            ViewBag.idservicio = id;
+            return View();
+        }
+
+
+        public ActionResult Guardar(string Comentario, int Calificacion1, int Id_Cliente, int Id_Servicio)
+        {
+            using (SqlConnection oconexion = new SqlConnection(cadena))
+            {
+                string query = "INSERT INTO Calificacion (Comentario, Calificacion, Id_cliente, Id_Servicio) VALUES (@comentario, @calificacion1, @id_cliente, @id_Servicio)";
+
+                SqlCommand cmd = new SqlCommand(query, oconexion);
+                cmd.Parameters.AddWithValue("@comentario", Comentario);
+                cmd.Parameters.AddWithValue("@calificacion1", Calificacion1);
+                cmd.Parameters.AddWithValue("@id_cliente", Id_Cliente);
+                cmd.Parameters.AddWithValue("@id_Servicio", Id_Servicio);
+
+                oconexion.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+
+            CambiarEstadoServicio(Id_Servicio);
+
+            return RedirectToAction("Index", "Calificacion", new { idCliente = Id_Cliente });
+        }
+
+        private void CambiarEstadoServicio(int? idServicio)
+        {
+            try
+            {
+                using (var db = new Model1())
+                {
+                    var servicio = db.Servicio.Find(idServicio);
+
+                    if (servicio != null)
+                    {
+                        servicio.Id_Estado_Servicio = 4;
+                        db.SaveChanges();
+                    }
+                    int idProblema = servicio.Id_Problema;
+                    var problema = db.Problema.Find(idProblema);
+                    if (problema != null)
+                    {
+                        problema.Estado = "Finalizado";
+                        db.SaveChanges();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult VerCalificacion(int id)
+        {
+            var vercali = db.Calificacion.Where(s => s.Id_Servicio == id).ToList();
+            ViewBag.calificacion = vercali;
+            return View();
+        }
+
     }
 }
--- Sistema/Models/Calificacion.cs
+++ Sistema/Models/Calificacion.cs
@@ -4,7 +4,9 @@ namespace SistemaArtemis.Models
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("Calificacion")]
     public partial class Calificacion
@@ -25,5 +27,70 @@ namespace SistemaArtemis.Models
         public virtual Servicio Servicio { get; set; }
 
         public virtual Cliente Cliente { get; set; }
+
+
+
+        public void Guardar()
+        {
+            try
+            {
+                using (var db = new Model1())
+                {
+                    if (this.Id_Calificacion > 0)
+                    {
+                        db.Entry(this).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        db.Entry(this).State = EntityState.Added;
+                    }
+                    db.SaveChanges();
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Calificacion Obtener(int id)
+        {
+            var califica = new Calificacion();
+            try
+            {
+                using (var db = new Model1())
+                {
+                    califica = db.Calificacion
+                        .Where(x => x.Id_Servicio == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return califica;
+        }
+
+        public List<Calificacion> Listar()
+        {
+            var tipousuario = new List<Calificacion>();
+            try
+            {
+                using (var db = new Model1())
+                {
+                    tipousuario = db.Calificacion.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return tipousuario;
+        }
+
+
     }
 }
